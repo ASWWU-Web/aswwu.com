f8a245dd3b63224951980b672436b134327f7128
Brock Haugen <brock.a.haugen@gmail.com>
2016-05-25 23:38:03 -0700

    reset config url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
 	title: "ASWWU",
 	description: "the Associated Student Body of Walla Walla University",
 	favicon: "images/mask_unknown.png",
-	server: "http://127.0.0.1:8888/",
+	server: "/server/",
 	files: {
 		js: [
 			"static/js/navigation.js",
